a4979d332197ea5b3f4c16749f2e3c1b6ca83dc7
EdVraz <88881326+EdVraz@users.noreply.github.com>
2023-06-05 20:00:11 +0200

    refactor: make it work in a way that makes me feel good

--- generate_images.py
+++ generate_images.py
@@ -32,7 +32,12 @@ async def generate_overview(s: Stats) -> None:
     Generate an SVG badge with summary statistics
     :param s: Represents user's GitHub statistics
     """
-    with open("templates/overview.svg", "r") as f:
+    if suffix := os.getenv("FILE_OUTPUT_SUFFIX"):
+        file = f"templates/overview{suffix}.svg"
+    else:
+        file = "templates/overview.svg"
+        
+    with open(file, "r") as f:
         output = f.read()
 
     output = re.sub("{{ name }}", await s.name, output)
@@ -45,7 +50,7 @@ async def generate_overview(s: Stats) -> None:
     output = re.sub("{{ repos }}", f"{len(await s.repos):,}", output)
 
     generate_output_folder()
-    with open("generated/overview.svg", "w") as f:
+    with open(file, "w") as f:
         f.write(output)
 
 
@@ -54,7 +59,13 @@ async def generate_languages(s: Stats) -> None:
     Generate an SVG badge with summary languages used
     :param s: Represents user's GitHub statistics
     """
-    with open("templates/languages.svg", "r") as f:
+
+    if suffix := os.getenv("FILE_OUTPUT_SUFFIX"):
+        file = f"templates/languages{suffix}.svg"
+    else:
+        file = "templates/languages.svg"
+
+    with open(file, "r") as f:
         output = f.read()
 
     progress = ""
@@ -86,7 +97,7 @@ fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg>
     output = re.sub(r"{{ lang_list }}", lang_list, output)
 
     generate_output_folder()
-    with open("generated/languages.svg", "w") as f:
+    with open(file, "w") as f:
         f.write(output)
 
 
